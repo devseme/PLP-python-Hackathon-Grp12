7717649903997fef4532cf7444fe8d2658a1e622
devseme <semeochenge@gmail.com>
2022-07-09 23:31:50 +0300

    finished on fats and carbs calories challenge

---
+++ calories.py
@@ -0,0 +1,15 @@
+# Day 3: Challenge Task Two (Day 3)
+# calculates the number of calories that result from the fat, using the following formula: 
+# Calories from fats fat grams * 9 
+# Calories from carbs carb grams * 4 
+
+# Calories from fats fat grams
+fatsGrams = int(input ("enter the number of fats grams:"))
+
+fatsCalories = fatsGrams * 9
+print (fatsCalories, "kcal")
+
+# Calories from carbs carb grams * 4 
+carbsGrams = int(input ("enter the number of carbs grams:"))
+carbsCalories =  carbsGrams * 4
+print (carbsCalories, "kcal")
